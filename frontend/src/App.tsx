83dd7dd8efa4b938bdcb04c3108b32c0b4089882
winmord <winmord@yandex.ru>
2026-06-11 19:57:11 +0300

    feature: finish task 1.2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
-import Keycloak, { KeycloakConfig } from 'keycloak-js';
+import Keycloak, { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js';
 import ReportPage from './components/ReportPage';
 
 const keycloakConfig: KeycloakConfig = {
-  url: process.env.REACT_APP_KEYCLOAK_URL,
-  realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
+    url: process.env.REACT_APP_KEYCLOAK_URL,
+    realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
 };
 
 const keycloak = new Keycloak(keycloakConfig);
+const initOptions: KeycloakInitOptions = {
+    pkceMethod: 'S256'
+}
 
 const App: React.FC = () => {
-  return (
-    <ReactKeycloakProvider authClient={keycloak}>
-      <div className="App">
-        <ReportPage />
-      </div>
-    </ReactKeycloakProvider>
-  );
+    return (
+        <ReactKeycloakProvider
+            authClient={keycloak}
+            initOptions={initOptions}
+        >
+            <div className="App">
+                <ReportPage />
+            </div>
+        </ReactKeycloakProvider>
+    );
 };
 
 export default App;
